6f2f1e914903446c97cd092d192460f6be7fc9f0
Md. Rishad Hossain <rishadmahmud11@gmail.com>
2024-12-19 00:02:56 +0600

    19-12-24

---
+++ number_sorter/script.js
@@ -0,0 +1,15 @@
+const sortButton = document.getElementById("sort");
+
+const sortInputArray = (event) => {
+  event.preventDefault();
+
+  const inputValues = [
+    ...document.getElementsByClassName("values-dropdown")
+  ].map((dropdown) => Number(dropdown.value));
+}
+
+const updateUI = (array = []) => {
+
+}
+
+sortButton.addEventListener("click", sortInputArray);
