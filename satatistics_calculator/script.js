612f812f5c7aa70711ee77966c76c33ec84c49b7
Md. Rishad Hossain <rishadmahmud11@gmail.com>
2024-12-22 21:14:39 +0600

    22-12-2024

--- satatistics_calculator/script.js
+++ satatistics_calculator/script.js
@@ -20,13 +20,48 @@ const calculate = () => {
   
   const mean = getMean(numbers);
   const median = getMedian(numbers);
+  const mode = getMode(numbers);
+  const range = getRange(numbers);
+  const variance = getVariance(numbers);
+  const standardDeviation = getStandardDeviation(numbers);
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
+  document.querySelector("#mode").textContent = mode;
+  document.querySelector("#range").textContent = range;
+  document.querySelector("#variance").textContent = variance;
+  document.querySelector("#standardDeviation").textContent = standardDeviation;
 }
 
 const getMode = (array) => {
     const counts = {};
     array.forEach((el) => counts[el] = counts[el] ? counts[el] + 1 : 1);
-    return counts;
+    if (new Set(Object.values(counts)).size === 1) {
+      return null;
+    }
+    const highest = Object.keys(counts).sort((a,b) => (counts[b]-counts[a]))[0];
+    const mode = Object.keys(counts).filter((el) => counts[el]===counts[highest]);
+    return mode.join(", ");
 }
+
+const getRange = (array) => {
+  const max = Math.max(...array);
+  const min = Math.min(...array);
+  return max-min;
+}
+const getVariance = (array) => {
+  const mean = getMean(array);
+
+  const variance = array.reduce((acc,el) => {
+    const difference = el-mean;
+    const squared = difference ** 2;
+    return acc+squared;
+  },0)/array.length();
+  return variance;
+}
+
+const getStandardDeviation = (array) => {
+  const variance = getVariance(array);
+  const standardDeviation = Math.sqrt(variance);
+  return standardDeviation;
+};
